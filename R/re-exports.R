310e49068f46cb9239726a210883316fb93b51b1
murman <mmuurr@gmail.com>
2023-10-25 14:20:22 +0200

    initial commit

---
+++ R/re-exports.R
@@ -0,0 +1,16 @@
+#' @export
+`%||%` <- rlang::`%||%`
+
+#' @export
+`%|%` <- rlang::`%|%`
+
+#' @export
+`%0%` <- vctrs::`%0%`
+
+#' @export
+`%===%` <- murlib.core::`%===%`
+
+#' @export
+`%!in%` <- murlib.core::`%!in%`
+
+
