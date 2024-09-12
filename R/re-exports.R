9c6fd4d7cc59cfd81aa46427705753ea36818ccf
Murat Tasan <mur@Murats-MacBook-Pro.local>
2024-09-12 17:25:48 +0200

    pkgdown

--- R/re-exports.R
+++ R/re-exports.R
@@ -1,23 +1,34 @@
+#' @title Infix operators
+#' @name reexports
+
+#' @rdname reexports
 #' @export
 `%@%` <- rlang::`%@%`
 
+#' @rdname reexports
 #' @export
 `%@%<-` <- rlang::`%@%<-`
 
+#' @rdname reexports
 #' @export
 `%||%` <- rlang::`%||%`
 
+#' @rdname reexports
 #' @export
 `%|%` <- rlang::`%|%`
 
+#' @rdname reexports
 #' @export
 `%0%` <- vctrs::`%0%`
 
+#' @rdname reexports
 #' @export
 `%===%` <- murlib.core::`%===%`
 
+#' @rdname reexports
 #' @export
 `%!in%` <- murlib.core::`%!in%`
 
+#' @rdname reexports
 #' @export
 `%notin%` <- murlib.core::`%notin%`
